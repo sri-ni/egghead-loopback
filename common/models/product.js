f7811116eb16664286e18530b70e3946e3e55864
Thirumalaa Srinivas <thrmlsrnvs@gmail.com>
2017-07-23 08:04:39 -0500

    Added model, relationships and remote method.

--- common/models/product.js
+++ common/models/product.js
@@ -1,5 +1,25 @@
 'use strict';
 
 module.exports = function(Product) {
+  /**
+   * Return true if input is larger than zero
+   * @param {number} quantity Number to validate
+   */
+  const validQuantity = quantity => Boolean(quantity > 0);
 
+  /**
+   * Buy this product
+   * @param {number} quantity Number of products to buy
+   * @param {Function(Error, object)} callback
+   */
+
+  Product.prototype.buy = function(quantity, callback) {
+    if (!validQuantity(quantity)) {
+      return callback(`Invalid quantity ${quantity}`);
+    }
+    const result = {
+      status: `You bought ${quantity} product(s)`,
+    };
+    callback(null, result);
+  };
 };
