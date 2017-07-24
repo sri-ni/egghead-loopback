84ac444fc42efdf9b01575ef680dabe0b13406d4
Thirumalaa Srinivas <thrmlsrnvs@gmail.com>
2017-07-24 09:39:34 -0500

    Validation rules added to model.

--- common/models/product.js
+++ common/models/product.js
@@ -22,4 +22,41 @@ module.exports = function(Product) {
     };
     callback(null, result);
   };
+
+  Product.validatesLengthOf('name', {
+    min: 3,
+    message: {
+      min: 'Name should be at least 3 characters',
+    },
+  });
+
+  Product.validatesUniquenessOf('name');
+
+  const positiveInteger = /^[0-9]*$/;
+
+  const validatePositiveInteger = function(err) {
+    if (!positiveInteger.test(this.price)) {
+      err();
+    }
+  };
+
+  // Product.validate('price', validatePositiveInteger, {
+  //   message: 'Price should be a positive integer',
+  // });
+
+  function validateMinimalPrice(err, done) {
+    const price = this.price;
+
+    process.nextTick(() => {
+      const minimalPriceFromDB = .99;
+      if (price < minimalPriceFromDB) {
+        err();
+      }
+      done();
+    });
+  }
+
+  Product.validateAsync('price', validateMinimalPrice, {
+    message: 'Price should be higher than the minimal price in the DB',
+  });
 };
